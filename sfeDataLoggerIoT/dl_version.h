09c8998e4365b1a87cebc2ae5daa933dd94e640e
Kirk Benell <kirk.benell@sparkfun.com>
2023-04-05 06:29:10 -0600

    header file for version info

---
+++ sfeDataLoggerIoT/dl_version.h
@@ -0,0 +1,24 @@
+
+
+#pragma once
+
+// Version information for the datalogger
+
+// Major version number
+#define kDLVersionNumberMajor		0
+
+// Minor version number
+#define kDLVersionNumberMinor		9
+
+// Point version number
+#define kDLVersionNumberPoint		1
+
+// Version string description
+#define kDLVersionDescriptor		"beta"
+
+// Build number - should come from the build system. If not, set default
+
+#ifndef BUILD_NUMBER
+#define BUILD_NUMBER 0
+#endif
+
