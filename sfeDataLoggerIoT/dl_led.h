315c9aebf550fccf324a6d1e269bea5b98959a1e
Kirk D. Benell <kirk.benell@sparkfun.com>
2023-04-20 17:32:54 -0600

    notice in files added

--- sfeDataLoggerIoT/dl_led.h
+++ sfeDataLoggerIoT/dl_led.h
@@ -1,5 +1,15 @@
 
-
+/*
+ *---------------------------------------------------------------------------------
+ *
+ * Copyright (c) 2022-2023, SparkFun Electronics Inc.  All rights reserved.
+ * This software includes information which is proprietary to and a
+ * trade secret of SparkFun Electronics Inc.  It is not to be disclosed
+ * to anyone outside of this organization. Reproduction by any means
+ * whatsoever is  prohibited without express written permission.
+ *
+ *---------------------------------------------------------------------------------
+ */
 
 // LED Display for the datalogger - led display in a task
 #pragma once
