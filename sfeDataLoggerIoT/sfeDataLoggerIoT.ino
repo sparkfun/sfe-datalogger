d1be78565371f741478988b9153733f2e4462a4d
Kirk Benell <kirk.benell@sparkfun.com>
2023-05-07 08:19:15 -0600

    final logic for button press event

--- sfeDataLoggerIoT/sfeDataLoggerIoT.ino
+++ sfeDataLoggerIoT/sfeDataLoggerIoT.ino
@@ -19,6 +19,7 @@
 #include "sfeDataLogger.h"
 
 #include "dl_led.h"
+#include "dl_ev.h"
 
 #define OPENLOG_ESP32
 #ifdef OPENLOG_ESP32
@@ -33,19 +34,12 @@
 
 sfeDataLogger  theDataLogger;
 
-
-// Button Things ...
-#define kButtonNone    0
-#define kButtonPress   1
-#define kButtonRelease 2
-
-static uint userButtonPressed = 0;
-static uint userButtonEvent=kButtonNone;
-uint32_t pressTime;
+static bool userButtonPressed;
+static uint userButtonEvent;
 
 void userButtonISR(void)
 {
-    userButtonEvent = digitalRead(BOOT_BUTTON) == HIGH ? kButtonRelease : kButtonPress;
+    userButtonEvent = digitalRead(BOOT_BUTTON) == HIGH ? kEventButtonRelease : kEventButtonPress;
 }
 
 
@@ -68,6 +62,10 @@ void setup() {
     // setup the button
     pinMode(BOOT_BUTTON, INPUT_PULLUP);
     attachInterrupt(BOOT_BUTTON, userButtonISR, CHANGE);
+
+    userButtonPressed = false;
+    userButtonEvent = kEventNone;
+
 }
 
 //---------------------------------------------------------------------
@@ -83,48 +81,20 @@ void loop() {
     if(flux.loop() && theDataLogger.ledEnabled == true)        // will return true if an action did something
         sfeLED.flash(sfeLED.Blue);
 
-    // TESTING of button state/proessing
-    // if (userButtonEvent != 0)
-    // {
-    //     Serial.printf("BUTTON EVENT %s\n\r", userButtonEvent == kButtonPress ? "Press" : "Relase");
-
-    //     if (userButtonEvent == kButtonPress)
-    //     {
-    //         pressTime = millis();
-    //         userButtonPressed = 1;
-    //     }
-    //     else
-    //     { 
-    //         dl_stopBlink();
-    //         Serial.printf("Button pressed for %.2f seconds \n\r", (millis()-pressTime)/1000.);
-    //         userButtonPressed = 0;
-    //     }
-    //     userButtonEvent=kButtonNone;
-    // }
-    // else if ( userButtonPressed)
-    // {
-    //     uint32_t delta = (millis() - pressTime)/1000;
-    //     if ( userButtonPressed == 1 && delta > 10 )
-    //     {
-    //         userButtonPressed++;
-    //         dl_startBlink(600);
-    //     }
-    //     else if (userButtonPressed == 2 && delta > 20)
-    //     {
-    //         userButtonPressed++;
-    //         dl_startBlink(200);
-    //     }
-    //     else if (userButtonPressed == 3 && delta > 30)
-    //     {
-    //         userButtonPressed++;
-    //         dl_stopBlink();
-    //         dl_ledBusy(true);
-    //     }else if (userButtonPressed == 4 && delta > 35)
-    //     {
-    //         Serial.printf("REBOOT!!");
-    //     }
-
-    // }
+    // user button event?
+    if (userButtonEvent != kEventNone)
+    {
+        dl_evButtonEvent(userButtonEvent);
+        userButtonPressed = (userButtonEvent == kEventButtonPress);
+        userButtonEvent = kEventNone;
+    }
+    // In a press event
+    else if (userButtonPressed)
+    {
+        // Reset the system?
+        if (dl_evButtonReset())
+            theDataLogger.resetDevice();
+    }
 
     delay(1);
 
