ad33e6dac7022aa6ba2ced2557999912f5f01e71
Kirk Benell <kirk.benell@sparkfun.com>
2023-05-08 16:35:15 -0600

    make a board header file

---
+++ sfeDataLoggerIoT/sfeDLBoard.h
@@ -0,0 +1,31 @@
+/*
+ *---------------------------------------------------------------------------------
+ *
+ * Copyright (c) 2022-2023, SparkFun Electronics Inc.  All rights reserved.
+ * This software includes information which is proprietary to and a
+ * trade secret of SparkFun Electronics Inc.  It is not to be disclosed
+ * to anyone outside of this organization. Reproduction by any means
+ * whatsoever is  prohibited without express written permission.
+ *
+ *---------------------------------------------------------------------------------
+ */
+
+// Board specific things for the DataLogger..
+#pragma once
+
+// Pins 
+
+// Das Boot button
+#define kDLBoardBootButton  0
+
+// 3v3 pin
+#define kDLBoardEn3v3_SW    32
+
+// LED Built in
+#define kDLBoardLEDBuiltin  25
+
+// RGB LED 
+#define kDLBoardLEDRGBBuiltin  26
+
+
+
