b8d4ba4c26927905f8ec8b0fe4d96ce9db7f5d03
Kirk Benell <kirk.benell@sparkfun.com>
2023-05-13 19:57:46 -0600

    add header to file

--- sfeDataLoggerIoT/sfeDLVersion.h
+++ sfeDataLoggerIoT/sfeDLVersion.h
@@ -1,4 +1,14 @@
-
+/*
+ *---------------------------------------------------------------------------------
+ *
+ * Copyright (c) 2022-2023, SparkFun Electronics Inc.  All rights reserved.
+ * This software includes information which is proprietary to and a
+ * trade secret of SparkFun Electronics Inc.  It is not to be disclosed
+ * to anyone outside of this organization. Reproduction by any means
+ * whatsoever is  prohibited without express written permission.
+ *
+ *---------------------------------------------------------------------------------
+ */
 
 #pragma once
 
