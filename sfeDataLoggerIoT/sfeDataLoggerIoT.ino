dcf79f5e400c3a04f0767b1716d42bd9e8d40316
Kirk Benell <kirk.benell@sparkfun.com>
2023-11-24 11:40:08 -0700

    formatter and cleanup

--- sfeDataLoggerIoT/sfeDataLoggerIoT.ino
+++ sfeDataLoggerIoT/sfeDataLoggerIoT.ino
@@ -6,50 +6,50 @@
  * trade secret of SparkFun Electronics Inc.  It is not to be disclosed
  * to anyone outside of this organization. Reproduction by any means
  * whatsoever is  prohibited without express written permission.
- * 
+ *
  *---------------------------------------------------------------------------------
  */
- 
+
 /*
- * Spark Framework demo - logging
- *   
+ * Flux Framework based logger.
+ *
  */
 
-// Spark framework 
-#include "sfeDataLogger.h"
+// Flux framework
 #include "sfeDLBoard.h"
+#include "sfeDataLogger.h"
 
 #include "sfeDLLed.h"
 
 // Our data logger application
-sfeDataLogger  theDataLogger;
+sfeDataLogger theDataLogger;
 
 //---------------------------------------------------------------------
 // Arduino Setup
 //
-void setup() {
-
+void setup()
+{
 
     pinMode(kDLBoardEn3v3_SW, OUTPUT); // Enable Qwiic power and I2C
     digitalWrite(kDLBoardEn3v3_SW, HIGH);
-    
+
     // Start up the framework
     flux.start();
 }
 
 //---------------------------------------------------------------------
-// Arduino loop - 
-void loop() {
+// Arduino loop -
+void loop()
+{
 
     ///////////////////////////////////////////////////////////////////
     // Flux
     //
     // Just call the Flux framework loop() method. Flux will manage
-    // the dispatch of processing to the components that were added 
+    // the dispatch of processing to the components that were added
     // to the system during setup.
-    if(flux.loop())        // will return true if an action did something
+    if (flux.loop()) // will return true if an action did something
         sfeLED.flash(sfeLED.Blue);
 
     delay(1);
-
 }
