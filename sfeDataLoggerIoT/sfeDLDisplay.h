535d710f47f26d2e74cbf3d67867e21361a639fd
Kirk Benell <kirk.benell@sparkfun.com>
2023-12-09 07:58:40 -0700

    made sure removed via #ifdefs; formatter

--- sfeDataLoggerIoT/sfeDLDisplay.h
+++ sfeDataLoggerIoT/sfeDLDisplay.h
@@ -11,83 +11,80 @@
  */
 #pragma once
 
-#include "sfeDLGraphx.h"
-#include "sfeDLGrWiFi.h"
+#ifdef THIS_IS_ENABLED
 #include "sfeDLGrBattery.h"
+#include "sfeDLGrWiFi.h"
+#include "sfeDLGraphx.h"
 
 #include <Flux/flxCoreEvent.h>
-#include <Flux/flxDevMicroOLED.h>
-#include <Flux/flxWiFiESP32.h>
 #include <Flux/flxDevMAX17048.h>
+#include <Flux/flxDevMicroOLED.h>
 #include <Flux/flxFSSDMMCard.h>
+#include <Flux/flxWiFiESP32.h>
 
-
-class sfeDLDisplay : public sfeDLGrView 
+class sfeDLDisplay : public sfeDLGrView
 {
 
-public:
+  public:
+    sfeDLDisplay() : _battery{nullptr}, _screen{nullptr}, _wifi{nullptr}, _card{nullptr}
+    {
+    }
 
-	sfeDLDisplay() : _battery{nullptr}, _screen{nullptr}, _wifi{nullptr}, _card{nullptr}
-	{
-	}
+    void initialize(flxDevMicroOLED *screen, flxWiFiESP32 *wifi, flxDevMAX17048 *fuel, flxFSSDMMCard *card)
+    {
+        _screen = screen;
+        _wifi = wifi;
+        _battery = fuel;
+        _card = card;
 
-	void initialize(flxDevMicroOLED *screen, flxWiFiESP32 *wifi, flxDevMAX17048 *fuel, flxFSSDMMCard *card)
-	{
-		_screen = screen;
-		_wifi = wifi;
-		_battery = fuel;
-		_card = card;
+        // Setup our WiFi graphic
+        _grWiFi.origin(4, 1);
+        _grWiFi.listenForUpdate(_on_wifi);
+        add(&_grWiFi);
+        _grWiFi.update();
 
-		// Setup our WiFi graphic
-		_grWiFi.origin(4, 1);
-		_grWiFi.listenForUpdate(_on_wifi);
-		add(&_grWiFi);
-		_grWiFi.update();
-		
-		// // Setup our Battery graphic
-		// _grBattery.origin(24, 1);
-		// _grBattery.listenForUpdate(_on_battery);
-		// add(&_grBattery);
-		// _grBattery.update();
+        // // Setup our Battery graphic
+        // _grBattery.origin(24, 1);
+        // _grBattery.listenForUpdate(_on_battery);
+        // add(&_grBattery);
+        // _grBattery.update();
+    }
 
-	}
+    void onGrUpdate(void)
+    {
+        if (!_screen)
+            return;
 
-	void onGrUpdate(void)
-	{
-		if (!_screen)
-			return;
+        if (_wifi)
+            _on_wifi.emit(_wifi->rating());
 
-		if (_wifi)
-			_on_wifi.emit(_wifi->rating());
+        if (_battery)
+        {
+            float batterySOC = _battery->getSOC();
+            uint level = batterySOC > 70. ? 4 : batterySOC > 40. ? 3 : batterySOC > 15. ? 2 : 1;
+            _on_battery.emit(level);
+        }
 
-		if (_battery)
-		{
-			float batterySOC = _battery->getSOC();
-			uint level = batterySOC > 70. ? 4 : batterySOC > 40. ? 3 : batterySOC > 15. ? 2 : 1;
-			_on_battery.emit(level);
-		}
+        draw(_screen);
+    }
 
-		draw(_screen);
-	}
+    void update(void)
+    {
+        if (_screen)
+            sfeDLGrView::update(_screen);
+    }
 
-	void update(void)
-	{
-		if (_screen)
-			sfeDLGrView::update(_screen);
-	}	
+  private:
+    flxDevMAX17048 *_battery;
+    flxDevMicroOLED *_screen;
+    flxWiFiESP32 *_wifi;
+    flxFSSDMMCard *_card;
 
-private:
+    flxSignalUInt _on_wifi;
+    sfeDLGrWiFi _grWiFi;
 
-	flxDevMAX17048 		*_battery;
-	flxDevMicroOLED 	*_screen;
-	flxWiFiESP32		*_wifi;
-	flxFSSDMMCard       *_card;
-
-	flxSignalUInt   	_on_wifi;
-	sfeDLGrWiFi	  		_grWiFi;
-
-	flxSignalUInt   	_on_battery;
-	sfeDLGrBattery	  	_grBattery;
-
+    flxSignalUInt _on_battery;
+    sfeDLGrBattery _grBattery;
 };
 
+#endif
