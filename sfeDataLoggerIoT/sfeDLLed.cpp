54c050eddc52eb97810d1372452be374ed508fe6
Kirk Benell <kirk.benell@sparkfun.com>
2026-01-20 14:42:53 -0700

    later versions of FastLED lib are causing the task to overflow the stack again - so adding more to stack size, and now specifying the fast LED version in the build script

--- sfeDataLoggerIoT/sfeDLLed.cpp
+++ sfeDataLoggerIoT/sfeDLLed.cpp
@@ -30,7 +30,7 @@ xTimerHandle hTimer;
 const uint16_t kTimerPeriod = 100;
 
 // A task needs a Stack - let's set that size
-#define kStackSize 1600
+#define kStackSize 1800
 #define kActivityDelay 50
 
 #define kLedColorOrder GRB
