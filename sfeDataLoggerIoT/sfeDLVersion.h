c4286601ffef20a6af02dd6288dfbb0a12c30b01
Kirk Benell <kirk.benell@sparkfun.com>
2026-02-18 13:52:37 -0700

    BETA 2

--- sfeDataLoggerIoT/sfeDLVersion.h
+++ sfeDataLoggerIoT/sfeDLVersion.h
@@ -22,7 +22,7 @@
 #define kDLVersionNumberPoint 0
 
 // Version string description
-#define kDLVersionDescriptor "Version 1.5.0 Beta 1"
+#define kDLVersionDescriptor "Version 1.5.0 Beta 2"
 
 // app name/class ID string
 #define kDLAppClassNameID "SFE-DATALOGGER-IOT"
